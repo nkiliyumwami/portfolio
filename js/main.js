ecfd6f4483d5d6dc3ce0490af48a82690d36166b
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-03-17 23:22:02 -0400

    Adding pagination, readmore button-like and condition of doantion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
+
+//Filter and sort list
+let options = {
+    valueNames: [ 'name', 'type', 'expertise' ]
+  };
+  
+  let donationList = new List('donations', options);
