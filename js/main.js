cc87f40f7cce2adbce3a6f49cd09386fe315cdc1
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-03-19 00:53:29 -0400

    Get rid of carousel, jumbotron.. etc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
-//Filter and sort list
-let options = {
-    valueNames: [ 'name', 'type', 'expertise' ]
-  };
-  
-  let donationList = new List('donations', options);
